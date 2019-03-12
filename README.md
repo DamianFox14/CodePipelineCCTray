8e19e07d94c51685234863b8a78f11ea337f4efa
damian.fox <damian.fox@ultra-as.com>
2019-03-12 16:39:43 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # CodePipelineCCTray
-This is an application that hosts a cc.xml file in the CCTray format created from AWS SDK calls to codepipeline
+This is an application that hosts a cc.xml file in the [CCTray](https://cctray.org/v1) format created from AWS SDK calls to 
+[CodePipeline](https://aws.amazon.com/codepipeline/)
+
+My use case for this this application was so that we could use [Nevergreen](https://github.com/build-canaries/nevergreen)
+as a build monitor with an AWS code pipeline backend.
+
+This application acts as a middle man between the two application calling out using the sdk to create a cc.xml file that
+nevergreen can process and use.
+
+In order to use this application you need to install the AWS CLI on the machine that you are using and configure that
+to use the account that the pipelines reside in. Once that is configured start my application.
+
+## Installation
+### AWS
+Install the AWS CLI as described [here](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html)
+
+Make sure that the user you are CLI'ing as has the following CodePipeline [permissions](https://docs.aws.amazon.com/codepipeline/latest/userguide/permissions-reference.html):
+* codepipeline:ListPipelines
+* codepipeline:ListPipelineExecutions
+* codepipeline:GetPipelineState
+
+### This App
+Check out the code and execute the following commands:
+* npm install
+* npm run start
+(You need to ensure this is ran on restart)
+
+
+Once this is completed on http://localhost:8080/cc.xml a file will be hosted which you can use for nevergreen or other tools.
+
+
+
+
---
+++ app/ccTrayCreator.js
@@ -0,0 +1,85 @@
+const builder = require('xmlbuilder');
+const AWS = require('aws-sdk');
+
+/**
+ * A restify function that will return the xml file created from the current AWS CodePipeline state.
+ *
+ * @param {Object} req - the request incoming from restify.
+ * @param {Object} res - the response to send to the client.
+ * @param {Object} next - pass on to the next part of the chain.
+ * @return {Promise<void>}
+ */
+exports.getCCxml = async function(req, res, next) {
+  const ccFile = {
+    Projects: {
+      Project: await createCCProjectList(),
+    },
+  };
+  const xml = builder.create(ccFile);
+  res.header('Content-Type', 'application/xml');
+  res.send(xml.end());
+  res.end();
+  next();
+};
+
+
+/**
+ * create a project to the current project list.
+ *
+ * @param {string} state - AWS state to transform into ccTray state.
+ * @param {string} name - Name of the project.
+ * @param {date} lastBuildTime - The time of the last build from the build history.
+ * @return {{"@name": *, "@webUrl": string, "@activity": string, "@lastBuildTime": string, "@lastBuildStatus": string}}
+ */
+function createProject(state, name, lastBuildTime) {
+  let activity = 'Sleeping';
+  let currentState = 'Success';
+
+  switch (state) {
+    case 'Succeeded':
+      activity = 'Sleeping';
+      currentState = 'Success';
+      break;
+    case 'InProgress':
+      activity = 'Building';
+      currentState = 'Success';
+      break;
+    case 'Failed':
+      activity = 'Sleeping';
+      currentState = 'Failure';
+      break;
+  }
+
+  const project = {
+    '@activity': activity,
+    '@lastBuildStatus': currentState,
+    '@webUrl': '',
+    '@name': name,
+    '@lastBuildTime': lastBuildTime.toISOString(),
+  };
+  return project;
+}
+
+
+/**
+ * Creates the list of projects in ccTray format.
+ *
+ * @return {Promise<Array>} list of projects
+ */
+async function createCCProjectList() {
+  const codepipeline = new AWS.CodePipeline();
+  const projectList = [];
+
+  const pipelineList = await codepipeline.listPipelines({}).promise();
+  for (let i = 0; i < pipelineList.pipelines.length; i++) {
+    const data = await codepipeline.getPipelineState({'name': pipelineList.pipelines[i].name}).promise();
+    const name = data.pipelineName;
+    const stages = data.stageStates;
+    const execData = await codepipeline.listPipelineExecutions({pipelineName: name}).promise();
+    for (let j = 0; j < stages.length; j++) {
+      projectList.push(createProject(stages[j].latestExecution.status, name + '-' + stages[j].stageName,
+          execData.pipelineExecutionSummaries[0].lastUpdateTime));
+    }
+    return projectList;
+  }
+}
